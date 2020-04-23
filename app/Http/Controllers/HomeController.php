a78cba69c5aea2ce5c20cef85ffeffd22f0647ef
aungsoeoo <aungsoeoo.94s@gmail.com>
2020-04-23 14:24:26 +0630

    add filter and total summary

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Visitor;
 use Carbon\Carbon;
+use DB;
 
 class HomeController extends Controller
 {
@@ -23,10 +24,35 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
         $todaycount = Visitor::whereDate('created_at', Carbon::today())->orderBy('branch','asc')->get();
-        $counts = Visitor::orderBy('created_at',"DESC")->paginate(10);
-        return view('home',compact('counts','todaycount'));
+
+        $data = new Visitor();
+
+        $date = date('Y-m-d', strtotime($request->date));
+
+        if($request->branch_id!=''){
+            $data = $data->where('branch',$request->branch_id);
+        }
+        if($request->date!=''){
+            $data = $data->whereDate('created_at',$date);
+        }
+
+        // if($request->branch_id=='' && $request->date==''){
+        //    $counts = $counts->whereDate('created_at', Carbon::today());
+        // }
+
+        $counts =$data->orderBy('branch','asc')->paginate(5);
+        $totalcount = $data->get();
+
+        $min_date = Visitor::select(DB::raw('MIN(created_at) as min_date'))->orderBy('created_at','asc')->get(['created_at'])->unique('created_at');
+
+        $hototal = Visitor::where('branch','=',1)->sum('count');
+        $l1total = Visitor::where('branch','=',2)->sum('count');
+        $l2total = Visitor::where('branch','=',3)->sum('count');
+        $summarytotal = Visitor::sum('count');
+
+        return view('home',compact('counts','todaycount','totalcount', 'min_date','hototal','l1total','l2total','summarytotal'))->with('i', ($request->input('page', 1) - 1) * 5);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    .col-lg-3,.col-md-2, .col-md-1{
+        margin-top: 5px !important;
+    }
+   
+</style>
 <div class="container">
     <div class="row" align="center">
         <h3>Customer Count During COVID-19</h3>
     </div>
     <br><br>
     <div class="row">
-        <p style="float: right;">{{ date('Y-m-d H:i:s') }}</p>
+        <h4>Today Customer Count</h4>
+        <p style="float: right;">{{  date('d-m-Y H:i:s A') }}</p>
     </div>
     <div class="row">
 
        @foreach ($todaycount as $data)
-        <div class="col-lg-4 col-6">
+        <div class="col-lg-3 col-6">
           <!-- small box -->
           <div class="small-box bg-info">
             <div class="inner" style="padding: 10px;">
@@ -38,10 +45,162 @@
           </div>
         </div>
         @endforeach
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-info">
+            <div class="inner" style="padding: 10px;">
+              <h4>
+                Total
+              </h4>
+              <br>
+              <div class="row">
+                <div class="col-md-4">
+                  <p>Customers</p>
+                </div>
+                <div class="col-md-6" align="right">
+                  <p>
+                    <?php 
+                        $total = 0;
+                        foreach ($todaycount as $key => $data) {
+                            $total = $total + $data->count;
+                        }
+                     ?>
+                    <strong>
+                       {{ $total }}
+                    </strong>
+                </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <br>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <h4>Total Customer  Count  ( {{ date('d-m-Y', strtotime( $min_date[0]->min_date)) }}  ~  {{  date('d-m-Y') }} )</h4>
+        </div>
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-success">
+            <div class="inner" style="padding: 10px;">
+              <h4>
+                HO
+              </h4>
+              <br>
+              <div class="row">
+                <div class="col-md-4">
+                  <p>Customers</p>
+                </div>
+                <div class="col-md-6" align="right">
+                  <p><strong>{{ number_format($hototal) }}</strong></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-success">
+            <div class="inner" style="padding: 10px;">
+              <h4>
+                Linn 1
+              </h4>
+              <br>
+              <div class="row">
+                <div class="col-md-4">
+                  <p>Customers</p>
+                </div>
+                <div class="col-md-6" align="right">
+                  <p><strong>{{ number_format($l1total) }}</strong></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-success">
+            <div class="inner" style="padding: 10px;">
+              <h4>
+                Linn 2
+              </h4>
+              <br>
+              <div class="row">
+                <div class="col-md-4">
+                  <p>Customers</p>
+                </div>
+                <div class="col-md-6" align="right">
+                  <p><strong>{{ number_format($l2total) }}</strong></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-success">
+            <div class="inner" style="padding: 10px;">
+              <h4>
+                Total Summary
+              </h4>
+              <br>
+              <div class="row">
+                <div class="col-md-4">
+                  <p>Customers</p>
+                </div>
+                <div class="col-md-6" align="right">
+                  <p>
+                   
+                    <strong>
+                       {{ number_format($summarytotal) }}
+                    </strong>
+                </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
     <br>
     <hr>
     <div class="container">
+        <?php 
+
+          $branch_id = isset($_GET['branch_id'])?$_GET['branch_id']:'';
+          $date = isset($_GET['date'])?$_GET['date']:'';
+      ?>
+        <form action="{{ url('home') }}" method="GET">
+            <div class="row">
+                <div class="col-md-2">
+                    <select name="branch_id" id="" class="form-control">
+                        <option value="">Select Branch</option>
+                        <option value="1" {{ ($branch_id==1)?'selected':'' }} >Head Office</option>
+                        <option value="2" {{ ($branch_id==2)?'selected':'' }}>Linn 1</option>
+                        <option value="3" {{ ($branch_id==3)?'selected':'' }}>Linn 2</option>
+                    </select>
+                </div>
+
+              <div class="col-md-2 text-center"> 
+                <input type="text" class="date form-control" placeholder="dd-mm-yyyy" name="date" value="{{ $date }}" />
+              </div>
+              <div class="col-md-1">
+                <input type="submit" class="btn btn-primary" value="Search">
+              </div>
+
+              <script type="text/javascript">  
+                   $('.date').datepicker({  
+                       format: 'dd-mm-yyyy'  
+                     }); 
+              </script> 
+            </div>
+        </form>
+
+        <br>
          <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
@@ -51,6 +210,7 @@
                             <table class="table">
                                 <thead>
                                     <tr>
+                                        <th>No</th>
                                         <th>Branch</th>
                                         <th>Count</th>
                                         <th>Date</th>
@@ -59,6 +219,7 @@
                                 <tbody>
                                     @foreach($counts as $data)
                                     <tr>
+                                        <td>{{ ++$i }}</td>
                                         <td>
 
                                             @if($data->branch==1)
@@ -73,10 +234,19 @@
                                         <td>{{ $data->created_at }}</td>
                                     </tr>
                                     @endforeach
-                                   
                                 </tbody>
-                            </table>
-                            
+                            </table> 
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12" style="text-align: center;">
+                             <?php 
+                                    $totalcustomer = 0;
+                                    foreach ($totalcount as $key => $data) {
+                                        $totalcustomer = $totalcustomer + $data->count;
+                                    }
+                                ?>
+                               <strong> Total Customer:  {{ number_format($totalcustomer) }} </strong>
+                            </div>
                         </div>
                         <!-- row / end -->
                         {!! $counts->render() !!}
